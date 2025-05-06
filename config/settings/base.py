fd69c06d0e4ba1abeb81e45ba36fd676f5a9ed4b
maduka paul <139281123+iam-madukapaul@users.noreply.github.com>
2025-05-06 11:23:15 +0100

    Added customer user model manager & user model

--- config/settings/base.py
+++ config/settings/base.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from os import getenv, path
 from loguru import logger
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
@@ -144,6 +145,7 @@ STATIC_ROOT = str(BASE_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "user_auth.User"
 
 LOGGING_CONFIG = None
 
